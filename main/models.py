cc378ac3d2475ead733b8f3cc8a416f3ca73bb28
DanTorBar <92780308+DanTorBar@users.noreply.github.com>
2025-01-02 10:38:55 +0100

    refactor: models refactor

--- main/models.py
+++ main/models.py
@@ -1,88 +1,88 @@
 from django.db import models
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
+class WorkoutCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.nombre
+        return self.name
 
-class CategoriaEjercicio(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.nombre
-
-
-class Musculo(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
+class ExerciseCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.nombre
+        return self.name
 
 
-class Etiqueta(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.nombre
-
-class Nivel(models.Model):
-    nombre = models.CharField(max_length=50, unique=True, verbose_name="Nivel")
+class Muscle(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.nombre
+        return self.name
 
 
-class Genero(models.Model):
-    nombre = models.CharField(max_length=50, unique=True, verbose_name="Género")
-
-    def __str__(self):
-        return self.nombre
-
-
-class ParteCuerpo(models.Model):
-    nombre = models.CharField(max_length=100, unique=True, verbose_name="Parte del Cuerpo")
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.nombre
+        return self.name
+
+class Level(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Level")
+
+    def __str__(self):
+        return self.name
 
 
-class Ejercicio(models.Model):
-    idEjercicio = models.AutoField(primary_key=True)
-    nombre = models.TextField(verbose_name='Nombre')
-    descripcion = models.TextField(verbose_name='Descripción')
-    categoriaEjercicio = models.ForeignKey(CategoriaEjercicio, on_delete=models.SET_NULL, null=True, verbose_name='Categoría')
-    musculosPri = models.ManyToManyField(Musculo, related_name='primarios', verbose_name='Músculos Primarios')
-    musculosSec = models.ManyToManyField(Musculo, related_name='secundarios', verbose_name='Músculos Secundarios')
+class Gender(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Gender")
+
+    def __str__(self):
+        return self.name
+
+
+class BodyParts(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Body Parts")
+
+    def __str__(self):
+        return self.name
+
+
+class Exercise(models.Model):
+    idExercise = models.AutoField(primary_key=True)
+    name = models.TextField(verbose_name='Exercise name')
+    description = models.TextField(verbose_name='Description')
+    exerciseCategory = models.ForeignKey(ExerciseCategory, on_delete=models.SET_NULL, null=True, verbose_name='Exercise Category')
+    priMuscles = models.ManyToManyField(Muscle, related_name='primary', verbose_name='Primary Muscles')
+    secMuscles = models.ManyToManyField(Muscle, related_name='secondary', verbose_name='Secondary Muscles')
     video = models.TextField(verbose_name='Video')
-    instrucciones = models.TextField(verbose_name='Instrucciones')
-    etiquetas = models.ManyToManyField(Etiqueta, verbose_name='Etiquetas')
+    instructions = models.TextField(verbose_name='Instructions')
+    tags = models.ManyToManyField(Tag, verbose_name='Tags')
 
     def __str__(self):
-        return self.nombre
+        return self.name
 
     class Meta:
-        ordering = ('nombre', )
+        ordering = ('name', )
 
-class Rutina(models.Model):
-    nombre = models.CharField(max_length=200, verbose_name="Nombre de la Rutina")
-    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, verbose_name="Categoría")
-    nivel = models.ForeignKey(Nivel, on_delete=models.SET_NULL, null=True, verbose_name="Nivel")
-    genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True, verbose_name="Género")
-    partes_cuerpo = models.ManyToManyField(ParteCuerpo, verbose_name="Partes del Cuerpo Trabajadas")
-    descripcion = models.TextField(verbose_name="Descripción")
+class Workout(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Workout name")
+    workoutCategory = models.ForeignKey(WorkoutCategory, on_delete=models.SET_NULL, null=True, verbose_name="Workout Category")
+    level = models.ForeignKey(Level, on_delete=models.SET_NULL, null=True, verbose_name="Level")
+    gender = models.ForeignKey(Gender, on_delete=models.SET_NULL, null=True, verbose_name="Gender")
+    bodyParts = models.ManyToManyField(BodyParts, verbose_name="Body Parts")
+    description = models.TextField(verbose_name="Description")
 
     # Relación con Ejercicios para cada día
-    dia_1 = models.ManyToManyField(Ejercicio, related_name="rutinas_dia_1", blank=True)
-    dia_2 = models.ManyToManyField(Ejercicio, related_name="rutinas_dia_2", blank=True)
-    dia_3 = models.ManyToManyField(Ejercicio, related_name="rutinas_dia_3", blank=True)
-    dia_4 = models.ManyToManyField(Ejercicio, related_name="rutinas_dia_4", blank=True)
-    dia_5 = models.ManyToManyField(Ejercicio, related_name="rutinas_dia_5", blank=True)
-    dia_6 = models.ManyToManyField(Ejercicio, related_name="rutinas_dia_6", blank=True)
-    dia_7 = models.ManyToManyField(Ejercicio, related_name="rutinas_dia_7", blank=True)
+    dia_1 = models.ManyToManyField(Exercise, related_name="rutinas_dia_1", blank=True)
+    dia_2 = models.ManyToManyField(Exercise, related_name="rutinas_dia_2", blank=True)
+    dia_3 = models.ManyToManyField(Exercise, related_name="rutinas_dia_3", blank=True)
+    dia_4 = models.ManyToManyField(Exercise, related_name="rutinas_dia_4", blank=True)
+    dia_5 = models.ManyToManyField(Exercise, related_name="rutinas_dia_5", blank=True)
+    dia_6 = models.ManyToManyField(Exercise, related_name="rutinas_dia_6", blank=True)
+    dia_7 = models.ManyToManyField(Exercise, related_name="rutinas_dia_7", blank=True)
 
     def __str__(self):
-        return self.nombre
+        return self.name
 
     class Meta:
-        ordering = ['nombre']
+        ordering = ['name']
