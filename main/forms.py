784cc12574587da845339987437620969a9e6823
DanTorBar <92780308+DanTorBar@users.noreply.github.com>
2025-01-07 17:00:30 +0100

    feat: user system implementation

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,26 @@
 #encoding:utf-8
 from django import forms
 from main.models import Exercise, Muscle, Workout
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 from itertools import chain
 
-   
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, label="Nombre")
+    last_name = forms.CharField(max_length=30, required=True, label="Apellido")
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+class EditUserForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
 class ExerciseTermSearchForm(forms.Form):
     term = forms.CharField(label="Término", widget=forms.TextInput, required=False)
 
