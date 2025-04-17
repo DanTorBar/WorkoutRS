edd989435079573882f47b9338c61b1dadd3cc56
DanTorBar <92780308+DanTorBar@users.noreply.github.com>
2025-04-17 17:32:46 +0200

    feat: DeepL translation service implementation on scrapping

--- WorkoutRS/settings.py
+++ WorkoutRS/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "threadedcomments",
-    "django_comments",
-    "django.contrib.sites",
+    # "threadedcomments",
+    # "django_comments",
+    # "django.contrib.sites",
     "main",
 ]
 
